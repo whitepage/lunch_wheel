a218921b531b6149d725ac797cffb7c2a044d97a
whitepage <white82x@gmail.com>
2021-11-03 11:29:24 +0900

    메뉴 업데이트

--- js/common.js
+++ js/common.js
@@ -2,214 +2,214 @@
 	function shuffle(array) { array.sort(() => Math.random() - 0.5); } 
 	
     var venues =  [
-        {
+		{
 			"name": "등촌샤브칼국수",
-			"type": null,
+			"type": "칼국수, 만두",
 			"desc": "고기추가 안하면 서운함"
 		  },
 		  {
 			"name": "후암편백",
-			"type": null,
+			"type": "일식당",
 			"desc": "편백찜도 있지만 샤브샤브가 더 맛있음"
 		  },
 		  {
 			"name": "유키사키",
-			"type": null,
+			"type": "유키사키",
 			"desc": "가츠동, 스테이키동 빼고는 맛이없음"
 		  },
 		  {
 			"name": "오제제",
-			"type": null,
+			"type": "돈가스",
 			"desc": "돈까스 맛집. But 10시에 대기 걸어야함"
 		  },
 		  {
 			"name": "바스버거",
-			"type": null,
+			"type": "햄버거",
 			"desc": "맛있는 수제버거. 토핑이 풍부함"
 		  },
 		  {
 			"name": "죠우",
-			"type": null,
+			"type": "유키사키",
 			"desc": "깔끔한 일본 가정식"
 		  },
 		  {
 			"name": "가마솥족발순대국",
-			"type": null,
+			"type": "한식",
 			"desc": "국물이 아주 맑은 순대국집! 맛집이라 웨이팅이 있을지도 몰라요"
 		  },
 		  {
 			"name": "후암연어식당",
-			"type": null,
+			"type": "퓨전음식",
 			"desc": "연어삼합이 베스트이지만 점심땐 초밥이랑 파스타도 있어요"
 		  },
 		  {
 			"name": "오지차돌박이",
-			"type": null,
+			"type": "육류,고기요리",
 			"desc": "차돌박이 정식 굿굿. 점심에도 고기를 구워먹을 수 있어요"
 		  },
 		  {
 			"name": "도동집",
-			"type": null,
+			"type": "한식",
 			"desc": "일명 이영자 맛집. 일찍가야 먹을 수 있는 곳!"
 		  },
 		  {
 			"name": "교동짬뽕",
-			"type": null,
+			"type": "중식당",
 			"desc": "해장이 필요할 때!"
 		  },
 		  {
 			"name": "대독장",
-			"type": null,
+			"type": "찌개, 전골",
 			"desc": "맛있는 김치찌개집! 세트메뉴가 좋아요"
 		  },
 		  {
 			"name": "서브웨이",
-			"type": null,
+			"type": "샌드위치",
 			"desc": "다이어트 결심이 드는 날?"
 		  },
 		  {
 			"name": "홍콩중국요리",
-			"type": null,
+			"type": "중식당",
 			"desc": "짜장면이 맛있는 곳"
 		  },
 		  {
 			"name": "메시야",
-			"type": null,
+			"type": "돈가스",
 			"desc": "다양한 돈까스를 만날 수 이는 곳. 대표메뉴는 김치돈까스!"
 		  },
 		  {
 			"name": "양귀비짬뽕",
-			"type": null,
+			"type": "중식당",
 			"desc": "짬뽕과 마늘탕수육이 맛있는 곳"
 		  },
 		  {
 			"name": "동해오징어보쌈",
-			"type": null,
+			"type": "오징어요리",
 			"desc": "매콤한게 땡기는날! 오징어보쌈과 오징어불고기가 맛있는 곳"
 		  },
 		  {
 			"name": "스시Zip",
-			"type": null,
+			"type": "초밥,롤",
 			"desc": "다양한 스시를 맛볼 수 있는 곳"
 		  },
 		  {
 			"name": "은하수닭갈비",
-			"type": null,
+			"type": "닭갈비",
 			"desc": "맛있는 철판닭갈비! 볶음밥 필수"
 		  },
 		  {
 			"name": "모모키친",
-			"type": null,
+			"type": "돈가스",
 			"desc": "깔끔한 돈까스 맛집"
 		  },
 		  {
 			"name": "미니네칼국수",
-			"type": null,
+			"type": "칼국수, 만두",
 			"desc": "칼제비와 들깨칼국수가 인기!"
 		  },
 		  {
 			"name": "충무칼국수",
-			"type": null,
+			"type": "칼국수, 만두",
 			"desc": "칼국수집인데 보쌈이 맛있는 곳"
 		  },
 		  {
 			"name": "약초마을",
-			"type": null,
+			"type": "백숙,삼계탕",
 			"desc": "괜히 몸보신 하고싶은 날, 삼계탕 한그릇"
 		  },
 		  {
 			"name": "마부마라탕",
-			"type": null,
+			"type": "중식당",
 			"desc": "마라탕과 마라샹궈가 맛있는 곳! 공기밥 무료 제공이 짱이쥬"
 		  },
 		  {
 			"name": "후암김밥",
-			"type": null,
+			"type": "분식",
 			"desc": "분식이 생각날 때! 김밥과 라면, 김치볶음밥이 아주 맛있어요"
 		  },
 		  {
 			"name": "우미우가",
-			"type": null,
+			"type": "한식",
 			"desc": "메뉴가 많지는 않지만 깔끔한 바싹 소불고기를 먹을 수 있는 곳"
 		  },
 		  {
 			"name": "모이정",
-			"type": null,
+			"type": "한식",
 			"desc": "로제돈까스가 있는 곳! 비빔국수와 국수도 있어요"
 		  },
 		  {
 			"name": "산소",
-			"type": null,
+			"type": "한식",
 			"desc": "수제비와 김치수제비, 제육덮밥 등이 인기인 곳! 가성비 굿굿"
 		  },
 		  {
 			"name": "카니발피자",
-			"type": null,
+			"type": "피자",
 			"desc": "바삭한 감자튀김과 치즈폭탄 피자가 훌륭한 곳! 파스타 메뉴도 많아서 여러명이 방문하기 좋아요"
 		  },
 		  {
 			"name": "제일제면소",
-			"type": null,
+			"type": "국수",
 			"desc": "임직원할인으로 애사심이 조금 생겨날 수 있는 곳. 면과 밥 종류가 많아요!"
 		  },
 		  {
 			"name": "더플레이스",
-			"type": null,
+			"type": "이탈리아음식",
 			"desc": "임직원할인으로 애사심이 조금 생겨날 수 있는 곳. 파스타, 피자, 스테이크 등 이탈리안이 생각날때!"
 		  },
 		  {
 			"name": "돈수백",
-			"type": null,
+			"type": "국밥",
 			"desc": "부산 돼지국밥이 생각날 때! 수육도 있어요~~"
 		  },
 		  {
 			"name": "신당동즉석떡볶이",
-			"type": null,
+			"type": "떡볶이",
 			"desc": "후암동에 몇 안되는 즉석떡볶이 집! 가게가 좁아서 소규모로 일찍 가야해요"
 		  },
 		  {
 			"name": "봉추찜닭",
-			"type": null,
+			"type": "찜닭",
 			"desc": "믿고먹는 봉추찜닭! 서울스퀘어에 있어요"
 		  },
 		  {
 			"name": "사위식당",
-			"type": null,
+			"type": "곱창,막창,양",
 			"desc": "낙곱새 맛집 사위식당! 서울스퀘어에 있어요"
 		  },
 		  {
 			"name": "토끼정",
-			"type": null,
+			"type": "일식당",
 			"desc": "서울역사에 있어서 시간 여유가 있는날이 좋겠어요!"
 		  },
 		  {
 			"name": "빌라 드 스파이시",
-			"type": null,
+			"type": "떡볶이",
 			"desc": "즉석떡볶이로 Flex 하고싶은 날!"
 		  },
 		  {
 			"name": "산들바다",
-			"type": null,
+			"type": "비빔밥",
 			"desc": "다양한 종류의 돌솥 비빔밥이 있는 곳!"
 		  },
 		  {
 			"name": "윤가네의정부부대찌개",
-			"type": null,
+			"type": "찌개, 전골",
 			"desc": "후암동 부대찌개 맛집!?"
 		  },
 		  {
 			"name": "정가네황태해장국",
-			"type": null,
+			"type": "한식",
 			"desc": "황태가 들어간 다양한 음식이 있는 곳! 황태미역국, 황태떡국, 황태해장국!"
 		  },
 		  {
 			"name": "만리장성",
-			"type": null,
+			"type": "중식당",
 			"desc": "중국음식이 땡길 때! 짜장면과 탕수육이 맛있는 곳!"
 		  },
 		  {
 			"name": "정선할매 곤드레밥",
-			"type": null,
+			"type": "한식",
 			"desc": "깔끔한 곤드레밥 정식!"
 		  }
     ];
@@ -316,7 +316,9 @@
 				wheel.timerHandle = 0;
 				wheel.angleDelta = 0;
 
-                if (console){ console.log((wheel.frames / duration * 1000) + " FPS"); }
+                if (console){ 
+					console.log((wheel.frames / duration * 1000) + " FPS");
+			 	}
 			}
 
 			/*
@@ -426,7 +428,7 @@
             winner = wheel.segments[i] || 'Choose at least 1 Venue';
 			ctx.fillText(winner, centerY, 380);
 
-			console.log(wheel.segments[i])
+			// console.log(wheel.segments[i]);
 		},
 
 		drawSegment : function(key, lastAngle, angle) {
